df29cf361f4edf4a8412851fbfd16f24f18d2780
Rukiye ÇAYIRBAĞ <92246250+rukiyecyrbg@users.noreply.github.com>
2024-02-05 18:42:43 +0300

    files upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Products from "../products/Products";
+
+function Home({ user }) {
+  return (
+    <>
+      <h1>Ana Sayfa</h1>
+      <Products user={user} />
+    </>
+  );
+}
+
+export default Home;
